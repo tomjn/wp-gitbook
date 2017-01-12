a73a20a8457d5475b80182c3c4c72b9ed0cc2e89
Tom J Nowell <contact@tomjn.com>
2017-01-12 23:45:38 +0000

    fix a duplication issue when more than one post is being shown

--- header-top.php
+++ header-top.php
@@ -1,7 +1,12 @@
-<div class="book-header" role="navigation">
-    <!-- Title -->
-    <h1>
-        <i class="fa fa-circle-o-notch fa-spin"></i>
-        <a href="<?php the_permalink(); ?>" ><?php the_title(); ?></a>
-    </h1>
-</div>
+<?php
+if ( !is_archive() ) {
+	?>
+	<div class="book-header" role="navigation">
+	    <!-- Title -->
+	    <h1>
+	        <i class="fa fa-circle-o-notch fa-spin"></i>
+	        <a href="<?php the_permalink(); ?>" ><?php the_title(); ?></a>
+	    </h1>
+	</div>
+	<?php
+}
--- index.php
+++ index.php
@@ -1,21 +1,33 @@
 <?php
 get_header();
 if ( have_posts() ) {
+	get_template_part( 'header', 'top' );
+	?>
+	<div class="page-wrapper" tabindex="-1" role="main">
+	<?php
 	while ( have_posts() ) {
 		the_post();
-		get_template_part( 'header', 'top' ); ?>
-
-		<div class="page-wrapper" tabindex="-1" role="main">
-			<div class="page-inner">
-					<section class="normal markdown-section">
-						<h1><?php the_title(); ?></h1>
-						<?php
-						the_content();
-						?>
-					</section>
-			</div>
+		?>
+		<div class="page-inner">
+				<section class="normal markdown-section">
+					<h1><?php
+					if ( is_archive() || is_home() ) {
+						?><a href="<?php the_permalink();?>"><?php
+					}
+					the_title();
+					if ( is_archive() || is_home() ) {
+						?></a><?php
+					}
+					?></h1>
+					<?php
+					the_content();
+					?>
+				</section>
 		</div>
 		<?php
 	}
+	?>
+	</div>
+	<?php
 }
 get_footer();
