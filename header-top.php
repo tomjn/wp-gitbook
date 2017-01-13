aade035011b16df6c650c16bae2878d24476809f
Tom J Nowell <contact@tomjn.com>
2017-01-13 00:16:41 +0000

    show different titles in the top header for archives and searches

--- header-top.php
+++ header-top.php
@@ -1,12 +1,27 @@
-<?php
-if ( !is_archive() ) {
-	?>
-	<div class="book-header" role="navigation">
-	    <!-- Title -->
-	    <h1>
-	        <i class="fa fa-circle-o-notch fa-spin"></i>
-	        <a href="<?php the_permalink(); ?>" ><?php the_title(); ?></a>
-	    </h1>
-	</div>
-	<?php
-}
+<div class="book-header" role="navigation">
+	<!-- Title -->
+	<h1>
+		<i class="fa fa-circle-o-notch fa-spin"></i>
+		<?php
+		if ( is_singular() ) {
+			?>
+			<a href="<?php the_permalink(); ?>" >
+				<?php the_title(); ?>
+			</a>
+			<?php
+		} else if ( is_search() ) {
+			?>
+			<a href="<?php echo esc_url( get_search_link() ); ?>" >
+				Search for: "<?php echo esc_html( get_search_query() ); ?>"
+			</a>
+			<?php
+		} else {
+			?>
+			<a href="#">
+				Archive
+			</a>
+			<?php
+		}
+		?>
+	</h1>
+</div>
